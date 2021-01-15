a78c095bcf05958b5721e3d0bb3184a254a060bc
Valeanu Victor <v.valeanu@yahoo.com>
2021-01-15 20:35:09 +0200

    Prepare Angular App for Production

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,7 +2,7 @@
 export const DIFFICULTIES: Set<string> = new Set(['Junior', 'Intermediate', 'Advanced']);
 export const CATEGORIES: Set<string> = new Set(['Java Core', 'Java 8+ Features', 'OOP', 'Docker', 'Angular2+']);
 
-export const API_URL = 'http://localhost:8080';
+export const API_URL = 'https://spring-hotinterviewquestions.herokuapp.com/';
 
 export const TOKEN = 'token'
 export const AUTHENTICATED_USER = 'authenticaterUser'
